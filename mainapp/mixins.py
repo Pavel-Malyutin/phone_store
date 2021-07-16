9f1312189734762a081eb29e99252c95a04c8185
User <wer@qer.3>
2021-07-16 13:15:42 +0300

    added pagination

--- mainapp/mixins.py
+++ mainapp/mixins.py
@@ -12,12 +12,15 @@ class CategoryDetailMixin(SingleObjectMixin):
         'smartphones': Smartphone,
     }
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, page=3, **kwargs):
         if isinstance(self.get_object(), Category):
             model = self.CATEGORY_SLUG2PRODUCT_MODEL[self.get_object().slug]
             context = super().get_context_data(**kwargs)
             context['categories'] = Category.objects.get_categories_for_left_sidebar()
             context['category_products'] = model.objects.all()
+            paginator = Paginator(model.objects.all(), 9)
+            products_paginator = paginator.page(page)
+            context.update({'category_products': products_paginator})
             return context
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.get_categories_for_left_sidebar()
